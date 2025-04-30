5f6988d8956c1b647c6f020db6b9df566800c141
subat <subat.yucel1@gmail.com>
2025-04-30 11:47:00 +0300

    clearTimeout added to day16 to prevent memory leaks and be a better solution

--- day16/solution.js
+++ day16/solution.js
@@ -1,9 +1,14 @@
 const timeLimit = function (fn, t) {
   return async function (...args) {
+    let timer;
     const limiter = new Promise((_, reject) => {
-      setTimeout(() => reject("Time Limit Exceeded"), t);
+      timer = setTimeout(() => reject("Time Limit Exceeded"), t);
     });
 
-    return Promise.race([limiter, fn(...args)]);
+    try {
+      return await Promise.race([limiter, fn(...args)]);
+    } finally {
+      clearTimeout(timer);
+    }
   };
 };
