2cbb284723a1710280871c0561b4e9608a60c574
subat <subat.yucel1@gmail.com>
2025-05-21 10:40:33 +0300

    day 29 completed

---
+++ day29/solution.js
@@ -0,0 +1,11 @@
+const ArrayWrapper = function (nums) {
+  this.nums = nums;
+};
+
+ArrayWrapper.prototype.valueOf = function () {
+  return this.nums.reduce((acc, curr) => acc + curr, 0);
+};
+
+ArrayWrapper.prototype.toString = function () {
+  return `[${this.nums.join(",")}]`;
+};
